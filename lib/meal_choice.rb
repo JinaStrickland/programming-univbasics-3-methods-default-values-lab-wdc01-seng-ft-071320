0e72e877d539313869ebd3429770bdb3b1d67db9
Jina Strickland <jina30@gmail.com>
2020-06-25 22:24:09 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,6 +6,6 @@ def meal_choice(veg1, veg2, protein = "tofu")
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice
+meal_choice()
 #meal_choice("carrots", "string beans", "lentils")
 #meal_choice("carrots", "string beans")
